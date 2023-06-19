7fd36976d4415f35db8b283835709be4fa9745ea
mightysodiq <muritalasodiqbab1@gmail.com>
2023-06-19 16:30:46 +0100

    webpages designing

--- Hotel/Controllers/HomeController.cs
+++ Hotel/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace Hotel.Controllers
             return View();
         }
 
+        public IActionResult About() 
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
